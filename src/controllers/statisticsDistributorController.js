aeb337a0ddcd6696e28da3bad9688678597ca0ca
TulioH <t.hernandez@abracol.com>
2021-04-26 11:50:35 -0500

    cambios

--- src/controllers/statisticsDistributorController.js
+++ src/controllers/statisticsDistributorController.js
@@ -4,7 +4,7 @@ const { Op } = require("sequelize");
 statisticsDistributorController = {};
 
 function PieDistributor(distribuidor) {
-  return `SELECT 	(SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 1 ) as Proceso,
+    return `SELECT 	(SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 1 ) as Proceso,
                   (SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 2 ) as Despachado,
                   (SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 3 ) as NoDespachado
           FROM 	gestiondiaria
@@ -13,94 +13,98 @@ function PieDistributor(distribuidor) {
 }
 
 function PieAsesor(distribuidor, asesor) {
-  return `SELECT 	
+    return `SELECT 	
   (SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 1 AND gestiondiaria.distribuidor = "${distribuidor}" AND gestiondiaria.asesordistribuidor = "${asesor}" ) as Proceso,
   (SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 2 AND gestiondiaria.distribuidor = "${distribuidor}" AND gestiondiaria.asesordistribuidor = "${asesor}" ) as Despachado,
   (SELECT COUNT(*) FROM gestiondiaria  WHERE gestiondiaria.idEstado = 3 AND gestiondiaria.distribuidor = "${distribuidor}" AND gestiondiaria.asesordistribuidor = "${asesor}" ) as NoDespachado`
 }
 
-statisticsDistributorController.getChartLineal = async (req, res) => {
-  const distribuidor = req.distribuidor;
-  const asesordistribuidor = req.asesor;
-  const profile = req.profile;
-  const { fecha } = req.body;
-  try {
+statisticsDistributorController.getChartLineal = async(req, res) => {
+    const distribuidor = req.distribuidor;
+    const asesordistribuidor = req.asesor;
+    const profile = req.profile;
+    const { fecha } = req.body;
+    try {
+        if (profile == 5) {
+            let data = await gestiondiaria.findAll({
+                attributes: ["id", "valorPedido", "ingresoFH"],
+                where: {
+                    distribuidor,
+                    ingresoFH: {
+                        [Op.substring]: fecha,
+                    },
+                },
+                order: [
+                    ["ingresoFH", "ASC"]
+                ],
+            });
+            return res.status(200).json({
+                data,
+                message: "Datos obtenidos correctamente",
+            });
+        }
+
+        if (profile == 4) {
+            let data = await gestiondiaria.findAll({
+                attributes: ["id", "valorPedido", "ingresoFH"],
+                where: {
+                    distribuidor,
+                    asesordistribuidor,
+                    asesordistribuidor,
+                    ingresoFH: {
+                        [Op.substring]: fecha,
+                    },
+                },
+                order: [
+                    ["ingresoFH", "ASC"]
+                ],
+            });
+            return res.status(200).json({
+                data,
+                message: "Datos obtenidos correctamente",
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message,
+        });
+    }
+};
+
+statisticsDistributorController.getChartPie = async(req, res) => {
+    const distribuidor = req.distribuidor;
+    const asesordistribuidor = req.asesor;
+    const queryDistributor = PieDistributor(distribuidor);
+    const queryAsesor = PieAsesor(distribuidor, asesordistribuidor);
+    const profile = req.profile;
+
     if (profile == 5) {
-      let data = await gestiondiaria.findAll({
-        attributes: ["id", "valorPedido", "ingresoFH"],
-        where: {
-          distribuidor,
-          ingresoFH: {
-            [Op.substring]: fecha,
-          },
-        },
-        order: [["ingresoFH", "ASC"]],
-      });
-      return res.status(200).json({
-        data,
-        message: "Datos obtenidos correctamente",
-      });
+        try {
+            let result = await db.sequelize.query(queryDistributor);
+            return res.status(200).json({
+                data: result[0],
+                message: "Datos obtenidos correctamente",
+            });
+        } catch (error) {
+            return res.status(500).json({
+                error: error.message,
+            });
+        }
     }
 
     if (profile == 4) {
-      let data = await gestiondiaria.findAll({
-        attributes: ["id", "valorPedido", "ingresoFH"],
-        where: {
-          distribuidor,
-          asesordistribuidor,
-          asesordistribuidor,
-          ingresoFH: {
-            [Op.substring]: fecha,
-          },
-        },
-        order: [["ingresoFH", "ASC"]],
-      });
-      return res.status(200).json({
-        data,
-        message: "Datos obtenidos correctamente",
-      });
-    }       
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message,
-    });
-  }
-};
-
-statisticsDistributorController.getChartPie = async (req, res) => {
-  const distribuidor = req.distribuidor;
-  const asesordistribuidor = req.asesor;
-  const queryDistributor = PieDistributor(distribuidor);
-  const queryAsesor = PieAsesor(distribuidor,asesordistribuidor);
-  const profile = req.profile;
-
-  if (profile == 5) {
-    try {
-      let result = await db.sequelize.query(queryDistributor);
-      return res.status(200).json({
-        data: result[0],
-        message: "Datos obtenidos correctamente",
-      });
-    } catch (error) {
-      return res.status(500).json({
-        error: error.message,
-      });
+        try {
+            let result = await db.sequelize.query(queryAsesor);
+            return res.status(200).json({
+                data: result[0],
+                message: "Datos obtenidos correctamente",
+            });
+        } catch (error) {
+            return res.status(500).json({
+                error: error.message,
+            });
+        }
     }
-  }
-
-  if (profile == 4) {
-    try {
-      let result = await db.sequelize.query(queryAsesor);
-      return res.status(200).json({
-        data: result[0],
-        message: "Datos obtenidos correctamente",
-      });
-    } catch (error) {
-      return res.status(500).json({
-        error: error.message,
-      });
-    }
-  }
 
 };
 
