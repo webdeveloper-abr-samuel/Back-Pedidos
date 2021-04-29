08e84718897d3d17a4f80b0900a685742cd366f0
Samuel-Abracol <79264880+webdeveloper-abr-samuel@users.noreply.github.com>
2021-04-29 09:14:51 -0500

    send email

--- src/routes/pedidos/messagesEmailRoutes.js
+++ src/routes/pedidos/messagesEmailRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const messagesEmailController = require('../../controllers').messagesEmailController;
 
 router.post("/", messagesEmailController.post);
+router.post("/emailMessage", messagesEmailController.postMessages);
 
 module.exports = router;
